81a2fd4e6deb2587f5ac4a183cd4d182e7eab7ab
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2023-10-01 01:14:25 +0530

    hello

---
+++ Data structures/Greedy Algorithms/Knapsack_problem.py
@@ -0,0 +1,17 @@
+def knapsack(products, capacity):
+    products.sort(key=lambda x: x[1] / x[0], reverse=True)
+    cur_weight = 0
+    cur_value = 0
+    for item in products:
+        item_weight, item_value = item
+        if cur_weight + item_weight < capacity:
+            cur_weight += item_weight
+            cur_value += item_value
+
+    return cur_value
+
+
+items = [(10, 60), (20, 100), (30, 120)]  # (weight, value) pairs
+knapsack_capacity = 50
+max_value = knapsack(items, knapsack_capacity)
+print("Maximum Value:", max_value)
