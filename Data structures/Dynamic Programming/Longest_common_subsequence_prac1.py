f040e3da6d0c9240703cd4cb30642ee76d9b51c5
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2023-11-02 07:51:00 +0530

    hi

---
+++ Data structures/Dynamic Programming/Longest_common_subsequence_prac1.py
@@ -0,0 +1,17 @@
+def lcs(string1, string2, n, m):
+    if n == 0 or m == 0:
+        return 0
+
+    elif string1[n-1] == string2[m-1]:
+        return 1 + lcs(string1, string2, n - 1, m - 1)
+
+    else:
+        return max(lcs(string1, string2, n - 1, m), lcs(string1, string2, n, m - 1))
+
+
+if __name__ == '__main__':
+    S1 = "abcdaf"
+    S2 = "acbcf"
+    print("length of the longest common subsequence is : ", lcs(S1, S2, len(S1), len(S2)))
+
+#%%
