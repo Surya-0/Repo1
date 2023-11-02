a69050040bf5fd6e2ef32949b7b9c0ca658e78e3
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2023-11-02 12:31:53 +0530

    hi

--- Data structures/Dynamic Programming/Longest_common_subsequence_recursion_prac1.py
+++ Data structures/Dynamic Programming/Longest_common_subsequence_recursion_prac1.py
@@ -2,7 +2,7 @@ def lcs(string1, string2, n, m):
     if n == 0 or m == 0:
         return 0
 
-    elif string1[n-1] == string2[m-1]:
+    elif string1[n - 1] == string2[m - 1]:
         return 1 + lcs(string1, string2, n - 1, m - 1)
 
     else:
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     S2 = "acbcf"
     print("length of the longest common subsequence is : ", lcs(S1, S2, len(S1), len(S2)))
 
-#%%
+# The time complexity is O(2^m*n) and the space complexity is O(1)
