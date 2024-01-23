c945302ea95dbb7512cab2041848976797da6dd3
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-01-23 06:36:51 +0530

    nlp hi

---
+++ Haskell_coding/prac1.hs
@@ -0,0 +1,33 @@
+-- -- Factorial function using recursion
+-- factorial :: Integer -> Integer
+-- factorial 0 = 1
+-- factorial n = n * factorial (n - 1)
+
+-- -- Main function
+-- main :: IO ()
+-- main = do
+--   putStrLn "Enter a number to calculate its factorial:"
+--   input <- getLine
+--   let number = read input :: Integer
+--   putStrLn $ "The factorial of " ++ show number ++ " is: " ++ show (factorial number)
+ 
+-- Multiplication function
+multiply :: Integer -> Integer -> Integer
+multiply a b = a * b
+
+-- -- Main function
+-- main :: IO ()
+-- main = do
+--   putStrLn "Enter the first number:"
+--   input1 <- getLine
+--   let num1 = read input1 :: Integer
+
+--   putStrLn "Enter the second number:"
+--   input2 <- getLine
+--   let num2 = read input2 :: Integer
+
+--   let result = multiply num1 num2
+--   putStrLn $ "The multiplication of " ++ show num1 ++ " and " ++ show num2 ++ " is: " ++ show result
+
+
+
